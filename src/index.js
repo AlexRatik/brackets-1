fdb7676bb38026660f762e99007a4be5527d2e04
AlexRatik <aleksandr-ratkevich@mail.ru>
2021-07-11 17:25:03 +0300

    feat: add index.js (solved)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let brackets = bracketsConfig.join("").replace(/\,/g, "");
+    let store = [];
+    let indexes = [];
+    for (let i = 0; i < str.length; i++) {
+        indexes.push(brackets.indexOf(str[i]));
+        if (indexes[i] % 2 === 0) {
+            if (
+                str[i] === brackets[indexes[i] + 1] &&
+                indexes[i] === store[store.length - 1]
+            ) {
+                store.pop();
+            } else {
+                store.push(indexes[i]);
+            }
+        } else {
+            if (store.pop() !== indexes[i] - 1) {
+                return false;
+            }
+        }
+    }
+    return store.length === 0;
+};
